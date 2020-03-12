cff87cc414dd093e25904c8499ebea4966d6aa23
Erik Aron Szabo <54311015+Erik-Aron-Szabo@users.noreply.github.com>
2020-03-12 09:21:31 -0400

    Add files via upload
    ver 1.0

---
+++ Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolTweet
+{
+    [Serializable]
+    public class Message
+    {
+        public string Username { get; set; } // Name
+        public string Text { get; set; } // Content
+
+        //public DateTime Timestamp { get; set; }
+
+        public Message(string user, string text)
+        {
+            Username = user;
+            Text = text;
+            //Timestamp = DateTime.Now;
+        }
+
+        public Message()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Username}: {Text}";
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"{Timestamp}: {Text}@{Username}";
+        //}
+
+
+    }
+}
