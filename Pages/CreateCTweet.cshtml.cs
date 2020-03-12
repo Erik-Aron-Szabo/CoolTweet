41463afbd5c30dbffbcc7a2542d0687f8986da89
Erik Aron Szabo <54311015+Erik-Aron-Szabo@users.noreply.github.com>
2020-03-12 09:42:06 -0400

    Add files via upload
    ver 1.0

---
+++ Pages/CreateCTweet.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoolTweet
+{
+    public class CreateCTweetModel : PageModel
+    {
+        public string Message { get; private set; } = "PageModel in C#";
+        public void OnGet()
+        {
+            Message += $"Server time is { DateTime.Now }";
+        }
+    }
+}
